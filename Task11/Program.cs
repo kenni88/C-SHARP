3ce7d3e21b1e2768e8fd831b981e5a735a70e9ff
Olga <kenni88@list.ru>
2023-03-01 20:34:38 +0300

    Added Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+
+
+int number = new Random().Next(100,1000);
+int firstDigit=number/100;
+int secondDigit=number%10;
+Console.WriteLine($"Случайное число отрезка {number}->{firstDigit}{secondDigit}");
