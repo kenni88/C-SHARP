86a4d86d5c7ee239d6ffd6df797e09dc4a366300
Olga <kenni88@list.ru>
2023-03-09 22:16:10 +0300

    Added Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход
+// число и выдает количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите целое положительное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int countDigit = CountDigit(number);
+Console.WriteLine($"В числе {number} -> {countDigit} цифр");
+
+int CountDigit(int num)
+{
+   int count = 0;
+    while(num > 0)
+    {
+     num = num / 10;
+     count++;
+    }
+    return count;
+}
