88b5dd1f50ebf91e19e17947f43b471220ed59e1
Olga <kenni88@list.ru>
2023-03-12 00:03:16 +0300

    Added Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход 
+// число N и выдает произведение чисел от 1 до N.
+// 4 -> 24
+//5 -> 120
+
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int multiple = Factorial(number);
+    Console.WriteLine($"Произведение чисел от 1 до {number}={multiple}");
+}
+else
+{
+    Console.WriteLine("Введите положительное число");
+}
+int Factorial(int num)
+{
+    int multi = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+             multi = multi * i;
+        }
+    }
+    return multi;
+}
