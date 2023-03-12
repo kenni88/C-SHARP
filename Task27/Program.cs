1bedfdc05308adc29b2951bb8814fa0b02bb4e1b
Olga <kenni88@list.ru>
2023-03-12 20:49:08 +0300

    Added Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("Введите целое положительное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumnumbers = SumNumbers(number);
+Console.WriteLine($"Сумма чисел ={sumnumbers}");
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    while (num>0)
+    {
+        sum += num % 10;
+        num = num /10;
+    }
+    return sum;
+}
