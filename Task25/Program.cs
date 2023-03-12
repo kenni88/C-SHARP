6c8d82ddf90af520a8c1c97e6c5b1c0a04a6d411
Olga <kenni88@list.ru>
2023-03-12 22:02:20 +0300

    Added Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.WriteLine("Введите первое число:");
+int powerBase = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int exponent = Convert.ToInt32(Console.ReadLine());
+
+if (ValidateExponent(exponent))
+{
+    System.Console.WriteLine($"Число{powerBase} в степени{exponent} равно {Power(powerBase,exponent)}");
+}
+
+int Power(int powBas, int exp)
+{
+    int power = 1;
+    for (int i =0; i<exp; i++)
+    {
+        power *= powBas;
+    }
+    return power;
+}
+
+bool ValidateExponent(int exp)
+{
+    if (exp<0)
+    {
+        System.Console.WriteLine("Показатель не должен быть меньше нуля");
+        return false;
+    }
+    return true;
+}
