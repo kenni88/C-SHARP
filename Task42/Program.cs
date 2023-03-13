56d76b04616b665415ebb1d6f68f023f9170df59
Olga <kenni88@list.ru>
2023-03-13 22:13:31 +0300

    Added Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+//десятичное число в двоичное.
+//45 -> 101101
+//3 -> 11
+//2 -> 10
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string DecimalToBinary(int n)
+{
+    string result=string.Empty;
+    while(n>0)
+    {
+        result=result+n%2;
+        n=n/2;
+    }
+    return result;
+}
+
+Console.WriteLine(DecimalToBinary);
