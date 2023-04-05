f32d207e29176e8787acfaf0ede7cc315f539e5d
Olga <kenni88@list.ru>
2023-04-05 20:52:12 +0300

    Added Task54

---
+++ Task54/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте двумерный массив.Напишите программу, которая упорядочит по убыванию
+// элементы каждой строки двумерного массива.
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(array2d);
+SortToLower(array2d);
+Console.WriteLine();
+PrintMatrix(array2d);
+
+
+
+ int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($" {matrix[i, j]}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void SortToLower(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
